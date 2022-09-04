34debb9794da995129ecb0f0847eb58e6e7985d9
AntonPlish <antonplish@gmail.com>
2022-09-04 10:49:07 +0300

    Commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -50,6 +50,21 @@ export class App extends Component {
     });
   };
 
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parseContacts = JSON.parse(contacts);
+  
+    if (parseContacts) {
+      this.setState({ contacts: parseContacts })
+    };
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
   render() {
     return (
       <div
